1cc433662e6054a2ca0deef3cd7d33b40bea899b
zawad ahmed <zawadahmed@zawads-macbook-pro.local>
2019-02-10 16:02:33 -0500

    Initial Commit

---
+++ Calculator February 10th/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator February 10th
+//
+//  Created by zawad ahmed on 2/10/19.
+//  Copyright © 2019 zawad ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Calculator February 10thTests/Calculator_February_10thTests.swift
@@ -0,0 +1,34 @@
+//
+//  Calculator_February_10thTests.swift
+//  Calculator February 10thTests
+//
+//  Created by zawad ahmed on 2/10/19.
+//  Copyright © 2019 zawad ahmed. All rights reserved.
+//
+
+import XCTest
+@testable import Calculator_February_10th
+
+class Calculator_February_10thTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
